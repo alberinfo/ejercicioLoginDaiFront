c2161f6b3b1a3eb3eba7489347b5e960d0204222
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-04 11:41:33 -0300

    First upload

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
+import axios from 'axios';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-web';
 
 export default function App() {
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [result, setResult] = useState("");
+
+  async function handle(){
+    try { 
+      const resp = await axios.post("http://localhost:8080/login", {
+        name: user,
+        pwd: pwd
+      });
+      setResult("OK");
+    } catch (e) {
+      setResult("Usuario o contraseña incorrectas");
+    }
+  }
+
+  useEffect(() => {
+    console.log("???????", result);
+  }, [result])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput placeholder="usuario" onChangeText={(username) => setUser(username)}/>
+      <TextInput secureTextEntry={true} placeholder="contraseña" onChangeText={(pwd) => setPwd(pwd)} />
+      <Button title="Enviar" onPress={handle}/>
+
+      <Text>{result}</Text>
     </View>
   );
 }
